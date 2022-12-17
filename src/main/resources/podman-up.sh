eafa00b42a4bc59345d944f48e3ec2da4a9bfc00
Steve Storck <steve973@gmail.com>
2022-12-17 16:04:16 -0500

    Added comments.

--- src/main/resources/podman-up.sh
+++ src/main/resources/podman-up.sh
@@ -4,6 +4,7 @@ rm -rf ./certs > /dev/null 2>&1
 mkdir ./certs
 pushd ./certs
 
+# create the CA extension file
 cat > test.ext <<EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
@@ -19,9 +20,13 @@ EOF
 openssl genrsa -out ./myCA.key 2048
 # generate the local CA cert
 openssl req -x509 -new -nodes -key ./myCA.key -sha384 -days 999 -out ./myCA.pem -subj "/C=XX/ST=Confusion/L=Somewhere/O=example/CN=CertificateAuthority"
+# create certificate signing request
 openssl req -newkey rsa:4096 -nodes -sha384 -keyout ./test.key -out ./test.csr -subj "/C=XX/ST=Confusion/L=Somewhere/O=example/CN=$(hostname)"
+# process the signing request and sign with the fake CA
 openssl x509 -req -in ./test.csr -CA ./myCA.pem -CAkey ./myCA.key -CAcreateserial -out ./test.crt -days 999 -sha384 -extfile ./test.ext
+# create a p12 keystore
 openssl pkcs12 -export -out test.p12 -name "$(hostname)" -inkey test.key -in test.crt -passout pass:test -passin pass:
+# create a jks truststore
 keytool -import -trustcacerts -noprompt -alias "$(hostname)" -ext san=dns:localhost,ip:127.0.0.1 -file ./myCA.pem -keystore ./truststore.jks -storepass changeit
 popd
 
