e4c4c9efe541e1c2cb594469d7c6aef80ec3693a
Steve Storck <steve973@gmail.com>
2022-12-17 10:13:38 -0500

    Got xpe and xpi chains working with maps, as well as snippets to decrease repetition for matchers.

--- src/main/java/org/example/caddyx509/GreetingController.java
+++ src/main/java/org/example/caddyx509/GreetingController.java
@@ -3,17 +3,19 @@ package org.example.caddyx509;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s! xpe: %s";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+							 @RequestHeader("X-ProxiedEntitiesChain") String xpechain) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name, xpechain));
 	}
 }
