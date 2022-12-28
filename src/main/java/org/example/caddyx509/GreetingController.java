a6a1ccbf95c8e25dfdce387f981b4da78c29c281
Steve Storck <steve973@gmail.com>
2022-12-28 18:22:32 -0500

    Better cert subject and issuer dn parsing.

--- src/main/java/org/example/caddyx509/GreetingController.java
+++ src/main/java/org/example/caddyx509/GreetingController.java
@@ -1,6 +1,10 @@
 package org.example.caddyx509;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s! forwarded cert info: %s";
+	private static final String template = "Hello, %s! cert subject: %s | cert issuer: %s";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
 							 @RequestHeader("X-Forwarded-Tls-Client-Cert-Info") String certInfo) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name, certInfo));
+		String decodedCertInfo = URLDecoder.decode(certInfo, StandardCharsets.UTF_8);
+		Pattern pattern = Pattern.compile("^Subject=\"(?<subject>.+)\";Issuer=\"(?<issuer>.+)\"$");
+		Matcher matcher = pattern.matcher(decodedCertInfo);
+		String subject = "Unknown";
+		String issuer = "Unknown";
+		if (matcher.matches()) {
+			subject = matcher.group("subject");
+			issuer = matcher.group("issuer");
+		}
+		return new Greeting(counter.incrementAndGet(), String.format(template, name, subject, issuer));
 	}
 }
